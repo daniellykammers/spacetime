1774305bd0493f68432e00520c8e7ad738904de6
daniellykammers <dany.kammers@hotmail.com>
2023-05-22 19:03:00 -0300

    feat(setup): Initial Setup

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'Spacetime',
+  description: 'Uma cápsula do tempo',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans text-gray-100 bg-gray-900`}>
+        {children}
+      </body>
+    </html>
+  )
+}
